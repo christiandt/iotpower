7f128623390f1ec9ecd5957c426cdba3357c7d78
Christian D. Tuen <mail@christiandt.com>
2017-09-13 21:58:59 +0200

    Move main to new power drop class

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from pyHS100 import SmartPlug, SmartPlugException
 import time, datetime
+from lookback import Lookback
+
 
 plug = SmartPlug("10.0.0.44")
+interval = 30
+look = Lookback(4)
 
-with open('logfiles/some.csv', 'wb') as f:
-    while True:
-        try:
-            stamp = str(datetime.datetime.now())
-            power = str(plug.get_emeter_realtime()["power"])
-            print stamp, power
-            f.write(stamp+","+power+"\n")
-            time.sleep(5)
-        except SmartPlugException:
-            pass
 
+while True:
+    try:
+        stamp = str(datetime.datetime.now())
+        power = float(plug.get_emeter_realtime()["power"])
+
+        look.add_value(power)
+        time.sleep(interval)
+
+    except SmartPlugException:
+        pass
